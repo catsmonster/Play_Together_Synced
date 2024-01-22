7d683bdb5640c97fc3640d3e5743e3351dfda2f3
catsmonster <shaked.brand@gmail.com>
2024-01-22 21:10:17 +0200

    added a basic implementation of a random code generation to share with partner, the code is being sent to the firebase realtime database to be read later by the partner. It shows a countdown until the token expires and is deleted from the database. Right now it still doesn't delete the token if you press cancel (even though it should).

--- addon.py
+++ addon.py
@@ -1,13 +1,57 @@
 import xbmcaddon
 import xbmcgui
+from resources.lib.firebase_handler import log_message, write_data_to_firebase, token_exists_in_firebase, \
+    cleanup_expired_tokens
+import random
+import string
+import threading
+import time
 
-from resources.lib.firebase_handler import log_message
- 
-addon       = xbmcaddon.Addon()
-addonname   = addon.getAddonInfo('name')
 
-# Set a string variable to use
-line1 = "Making a change without restarting Kodi"
+def show_token_dialog(token, duration=300):
+    def countdown():
+        # Countdown logic in a separate thread
+        for i in range(duration, 0, -1):
+            time.sleep(1)
+        cleanup_expired_tokens()  # Delete expired tokens after countdown
 
-# Launch a dialog box in kodi showing the string variable 'line1' as the contents
-xbmcgui.Dialog().ok(addonname, line1)
+    dialog = xbmcgui.DialogProgress()
+    dialog.create('Token Information')
+
+    # Start the countdown in a separate thread
+    countdown_thread = threading.Thread(target=countdown)
+    countdown_thread.start()
+
+    for i in range(duration, -1, -1):
+        if dialog.iscanceled():  # Break the loop if user cancels the dialog
+            break
+        percent = int((duration - i) / duration * 100)
+        mins, secs = divmod(i, 60)
+        time_format = '{:02d}:{:02d}'.format(mins, secs)
+        message = f'Send this to the other partner: {token}\nExpires in: {time_format}'
+        dialog.update(percent, message)
+        time.sleep(1)
+
+    dialog.close()
+
+
+def generate_short_token(length=6):
+    """Generate a random alphanumeric token (lowercase and digits)."""
+    characters = string.ascii_lowercase + string.digits
+    generated_token = ''.join(random.choice(characters) for i in range(length))
+    return generated_token
+
+
+# Generate a token
+token = generate_short_token()
+while token_exists_in_firebase(token):
+    log_message('Token already exists in firebase, generating a new one')
+    token = generate_short_token()
+
+data = {'timestamp': int(time.time())}  # Current Unix time in seconds
+write_data_to_firebase(token, data)
+
+# Usage
+
+addon = xbmcaddon.Addon()
+show_token_dialog(token, 300)
--- resources/lib/firebase_handler.py
+++ resources/lib/firebase_handler.py
@@ -2,6 +2,7 @@ import xbmc
 import xbmcaddon
 import os
 import sys
+import time
 addon_base_path = xbmcaddon.Addon().getAddonInfo('path')
 lib_path = os.path.join(addon_base_path, 'resources', 'lib')
 sys.path.append(lib_path)
@@ -9,26 +10,36 @@ import requests
 import json
 
 firebase_url = 'https://play-together-sync-default-rtdb.europe-west1.firebasedatabase.app/'
-# Writing Data to Firebase
-data = {'name': 'John Doe', 'location': 'City'}
-response = requests.put(firebase_url + 'users/john_doe.json', json.dumps(data))
-if response.status_code == 200:
-    xbmc.log("Data written successfully")
-else:
-    xbmc.log("Failed to write data")
-
-# Reading Data from Firebase
-response = requests.get(firebase_url + 'users/john_doe.json')
-if response.status_code == 200:
-    data = response.json()
-    xbmc.log(f'The added data is:\n{data}')
-else:
-    xbmc.log("Failed to read data")
 
 
+def token_exists_in_firebase(token):
+    cleanup_expired_tokens()
+    response = requests.get(firebase_url + f'tokens/{token}.json')
+    if response.status_code == 200:
+        data = response.json()
+        if data is not None:
+            return True
+    return False
+
+
+def write_data_to_firebase(token, data):
+    response = requests.put(firebase_url + f'tokens/{token}.json', json.dumps(data))
+    if response.status_code == 200:
+        return True
+    return False
+
+
+def cleanup_expired_tokens(expiration_seconds=300):
+    current_time = int(time.time())
+    response = requests.get(f'{firebase_url}/tokens.json')
+    tokens = response.json()
+
+    if tokens:
+        for token, data in tokens.items():
+            if current_time - data['timestamp'] > expiration_seconds:
+                requests.delete(f'{firebase_url}/tokens/{token}.json')
 
 
 def log_message(message, level=xbmc.LOGDEBUG):
     addon_id = xbmcaddon.Addon().getAddonInfo('id')
     xbmc.log(f'[{addon_id}] {message}', level)
-
